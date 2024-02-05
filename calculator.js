e5a30a57fa4463132c26934fde928d62156fe982
kailermai <kailermaioff@gmail.com>
2024-02-05 16:59:18 +0800

    created initial files with basic math functions

---
+++ calculator.js
@@ -0,0 +1,23 @@
+var firstNum;
+var secondNum;
+var operator;
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function operatorFunc(firstNum, secondNum, operator) {
+  add(firstNum, secondNum);
+}
