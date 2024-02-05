aee14755e036e2439e0e8ce5d1527292fcaccb22
kailermai <kailermaioff@gmail.com>
2024-02-05 19:17:15 +0800

    added buttons to each div

--- calculator.js
+++ calculator.js
@@ -1,6 +1,7 @@
 var firstNum;
 var secondNum;
 var operator;
+var displayText;
 
 function add(num1, num2) {
   return num1 + num2;
@@ -20,4 +21,14 @@ function divide(num1, num2) {
 
 function operatorFunc(firstNum, secondNum, operator) {
   add(firstNum, secondNum);
+}
+
+const display = document.querySelector(".display");
+display.textContent = displayText;
+
+for (let i = 0; i <= 9; i++) {
+  i = document.querySelector(".button." + i);
+  i.addEventListener("click", () => {
+    displayText += i;
+  });
 }
