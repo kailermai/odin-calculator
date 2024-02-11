e8a3eb096cbb5a7de4dbd11bfe9be9fac04a975b
kailermai <kailermaioff@gmail.com>
2024-02-11 19:24:00 +0800

    fixed a bug where pressing on a number with a result in displaytext will cause an addition of the number and the result, changed it to adding an additional number to the end of the displaytext

--- calculator.js
+++ calculator.js
@@ -64,57 +64,57 @@ display.textContent = displayText;
 //Adding each number to display text
 const num0 = document.querySelector("#zero");
 num0.addEventListener("click", () => {
-  displayText += 0;
+  displayText += "0";
   display.textContent = displayText;
 });
 const num1 = document.querySelector("#one");
 num1.addEventListener("click", () => {
-  displayText += 1;
+  displayText += "1";
   display.textContent = displayText;
 });
 const num2 = document.querySelector("#two");
 num2.addEventListener("click", () => {
-  displayText += 2;
+  displayText += "2";
   display.textContent = displayText;
 });
 const num3 = document.querySelector("#three");
 num3.addEventListener("click", () => {
-  displayText += 3;
+  displayText += "3";
   display.textContent = displayText;
 });
 const num4 = document.querySelector("#four");
 num4.addEventListener("click", () => {
-  displayText += 4;
+  displayText += "4";
   display.textContent = displayText;
 });
 
 const num5 = document.querySelector("#five");
 num5.addEventListener("click", () => {
-  displayText += 5;
+  displayText += "5";
   display.textContent = displayText;
 });
 
 const num6 = document.querySelector("#six");
 num6.addEventListener("click", () => {
-  displayText += 6;
+  displayText += "6";
   display.textContent = displayText;
 });
 
 const num7 = document.querySelector("#seven");
 num7.addEventListener("click", () => {
-  displayText += 7;
+  displayText += "7";
   display.textContent = displayText;
 });
 
 const num8 = document.querySelector("#eight");
 num8.addEventListener("click", () => {
-  displayText += 8;
+  displayText += "8";
   display.textContent = displayText;
 });
 
 const num9 = document.querySelector("#nine");
 num9.addEventListener("click", () => {
-  displayText += 9;
+  displayText += "9";
   display.textContent = displayText;
 });
 
